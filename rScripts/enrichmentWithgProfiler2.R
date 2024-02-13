a0ba0bdc40dfe4df45640d866a5a25710f2630ff
Jianying Li <li11@almbp02012678.local.niehs.nih.gov>
2024-02-13 16:49:27 -0500

    from my work mac, feb13

--- rScripts/enrichmentWithgProfiler2.R
+++ rScripts/enrichmentWithgProfiler2.R
@@ -1,5 +1,7 @@
+##=========================================================
+##  This is sample code in the original paper
 ## https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7859841/
-## enrichment with gprofiler2
+## enrichment with gprofiler2 
 library(DESeq2)
 library(airway)
 library(gprofiler2)
@@ -27,7 +29,9 @@ down = subset(results_sig, log2FoldChange < 0)
 gp_up = gost(row.names(up), organism = "hsapiens")
 gp_down = gost(row.names(down), organism = "hsapiens") 
 
-
+head(gp_up$result)
+dim(up)
+dim(gp_up$result)
 table(gp_up$result$source)
 
 
@@ -38,6 +42,8 @@ up_ordered = up[order(up$log2FoldChange, decreasing = TRUE),]
 # ordered enrichment analysis
 gp_up_ordered = gost(row.names(up_ordered), organism = "hsapiens",
                      ordered_query = TRUE)
+
+dim(gp_up_ordered$result)
 head(gp_up_ordered$result, 8)
 
 table(gp_up_ordered$result$source)
@@ -68,5 +74,74 @@ results_genes = gconvert(row.names(results), organism = "hsapiens",
 head(results_genes)
 
 
+results_df = as.data.frame(results)
+results_df$Ensembl_id = row.names(results_df)
+results_df = results_df[order(results_df$padj),]
 
+# add the gene names
+results_df = merge(results_df,
+                   results_genes[,c("input", "target", "name", "description")],
+                   by.x = "Ensembl_id", by.y = "input")
+
+# save the results to a tsv file
+write.table(results_df, file = "/Users/li11/myGit/scRNAseqYouTuber/sampleAnalysisResults/DESeq2_results.tsv", sep = "\t",
+            quote = F, row.names = F)
+
+
+##.  Integrating with external tools for visualisations
+
+library(clusterProfiler)
+library(enrichplot)
+library(DOSE) # needed to convert to enrichResult object
+
+up_names = gconvert(row.names(up))
+down_names = gconvert(row.names(down))
+
+# enrichment analysis using gene names
+multi_gp = gost(list("up-regulated" = up_names$name,
+                     "down-regulated" = down_names$name), multi_query = FALSE, evcodes = TRUE)
+
+
+# modify the g:Profiler data frame
+gp_mod = multi_gp$result[,c("query", "source", "term_id",
+                            "term_name", "p_value", "query_size", 
+                            "intersection_size", "term_size", 
+                            "effective_domain_size", "intersection")]
+
+gp_mod$GeneRatio = paste0(gp_mod$intersection_size,  "/", gp_mod$query_size)
+gp_mod$BgRatio = paste0(gp_mod$term_size, "/", gp_mod$effective_domain_size)
+names(gp_mod) = c("Cluster", "Category", "ID", "Description", "p.adjust", 
+                  "query_size", "Count", "term_size", "effective_domain_size", 
+                  "geneID", "GeneRatio", "BgRatio")
+gp_mod$geneID = gsub(",", "/", gp_mod$geneID)
+
+row.names(gp_mod) = paste0(gp_mod$ID, "", gp_mod$Cluster)
+
+# define as compareClusterResult object
+gp_mod_cluster = new("compareClusterResult", compareClusterResult = gp_mod)
+
+# define as enrichResult object
+gp_mod_enrich  = new("enrichResult", result = gp_mod)
+enrichplot::dotplot(gp_mod_cluster)
+
+
+barplot(gp_mod_enrich, showCategory = 40, font.size = 6) + 
+  ggplot2::facet_grid(~Cluster) +
+  ggplot2::ylab("Intersection size")
+
+
+gostres = gost(query = list("up-regulated" = row.names(up)),
+               evcodes = TRUE, multi_query = FALSE,
+               sources = c("GO", "REAC", "MIRNA", "CORUM", "HP", "HPA", "WP"))
+gem = gostres$result[,c("term_id", "term_name", "p_value", "intersection")]
+colnames(gem) = c("GO.ID", "Description", "p.Val", "Genes")
+gem$FDR = gem$p.Val
+gem$Phenotype = "+1"
+gem = gem[,c("GO.ID", "Description", "p.Val", "FDR", "Phenotype", "Genes")]
+# saving the GEM file
+write.table(gem, file = "/Users/li11/myGit/scRNAseqYouTuber/sampleAnalysisResults/gProfiler_gem.txt", sep = "\t", quote = F, row.names = F)
+
+download.file(
+  url = "http://biit.cs.ut.ee/gprofiler/static/gprofiler_full_hsapiens.ENSG.gmt", 
+  "/Users/li11/myGit/scRNAseqYouTuber/sampleAnalysisResults/gProfiler_full_hs_ensg.gmt")
 
