2327d6930eaf8dbaa08d48156204b32d123cb5e7
Li, Jianying (NIH/NIEHS) [C] <jianying.li@nih.gov>
2025-04-04 08:53:00 -0400

    Attempt to commit

--- rScripts/FunGenomicsEnrichment.R
+++ rScripts/FunGenomicsEnrichment.R
@@ -4,6 +4,9 @@
 library(enrichplot)
 library(clusterProfiler)
 library(org.Hs.eg.db)
+library(org.Mm.eg.db)
+
+
 
 enrichment <- function (x, y){
   plot = enrichGO(
@@ -17,3 +20,45 @@ enrichment <- function (x, y){
     minG
   )
 }
+enrichment.Mm <- function (x, y){
+  plot = enrichGO(
+    x,
+    org.Mm.eg.db,
+    keyType = "ENTREZID",
+    ont = y,
+    pvalueCutoff = 0.05,
+    pAdjustMethod = "BH",
+    qvalueCutoff = 0.2,
+    minGSSize = 10,
+    maxGSSize = 500,
+    readable = FALSE,
+    pool = FALSE
+  )
+  dotplot(plot)
+}
+enrichment.Hs <- function (x, y){
+  plot = enrichGO(
+    x,
+    org.Hs.eg.db,
+    keyType = "ENTREZID",
+    ont = y,
+    pvalueCutoff = 0.05,
+    pAdjustMethod = "BH",
+    qvalueCutoff = 0.2,
+    minGSSize = 10,
+    maxGSSize = 500,
+    readable = FALSE,
+    pool = FALSE
+  )
+  dotplot(plot)
+}
+
+data(geneList, package = "DOSE")
+de <- names(geneList)[1:100]
+yy <- enrichGO(de, 'org.Hs.eg.db', ont="BP", pvalueCutoff=0.01)
+head(yy)
+dotplot(yy)
+
+
+enrichment.Mm (DEGs, "MF")
+
